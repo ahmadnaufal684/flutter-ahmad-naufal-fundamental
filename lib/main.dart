0bf7bdc5d23ee7a1d6aedf1e19cfac5f8a7b6d23
ahmadnaufal684 <kamadnvl@gmail.com>
2023-04-07 13:57:31 +0700

    mini projek 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,74 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  TextEditingController controller1 = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
+        home: Scaffold(
+      appBar: AppBar(
         backgroundColor: Colors.blue,
         title: Text('Calculator'),
-      )),
-    );
+      ),
+      body: Column(
+        children: [
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Result : ',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black))),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+              padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+              child: TextField(
+                controller: controller1,
+                decoration: InputDecoration(
+                    labelText: 'Enter first number',
+                    filled: true,
+                    fillColor: Colors.black12),
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+              padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+              child: TextField(
+                controller: controller2,
+                decoration: InputDecoration(
+                    labelText: 'Enter second number',
+                    filled: true,
+                    fillColor: Colors.black12),
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text('ADD')),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('SUBSTRACT')),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('MULTIPLY')),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('DIVIDE'))
+                ],
+              ))
+        ],
+      ),
+    ));
   }
 }
